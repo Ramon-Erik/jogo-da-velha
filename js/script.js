f675bddb2fd4e072ab9abcfc4e030dff4445cd50
Ramon <84084404+Ramon-Erik@users.noreply.github.com>
2023-07-05 23:00:10 -0300

    examinando tabuleiro - colunas

--- js/script.js
+++ js/script.js
@@ -22,17 +22,27 @@ function examinarTabuleiro() {
     for (let i = 0; i < 9; i++) {
         valoresTabuleiro.push(btns[i].children[0].innerText)
     }
-    // horizontal (linhas 1, 2 e 3)
+    // checar linhas 1, 2 e 3
     for (let i = 0; i < 9; i+=3) {
         if (valoresTabuleiro[i] !== '' && valoresTabuleiro[i] === valoresTabuleiro[i+1] && valoresTabuleiro[i] === valoresTabuleiro[i+2]) {
             console.log(`Examinando linha ${i}: ${valoresTabuleiro[i]} - ${valoresTabuleiro[i+1]} - ${valoresTabuleiro[i+2]}`)
             for (let id = i; id <= i+2; id++) {
-                // console.log(`id: ${id} | ${btns[id].classList.add('marcado')}`)
                 btns[id].classList.add('marcado')
             }
             break
         }
-    
+    }
+    // checar colunas 1, 2 e 3
+    for (let i = 0; i < 3; i++) {
+        if (valoresTabuleiro[i] !== '' &&
+        valoresTabuleiro[i] === valoresTabuleiro[i+3] &&
+        valoresTabuleiro[i] === valoresTabuleiro[i+6]) {
+            console.log(`Examinando coluna ${i}: ${valoresTabuleiro[i]} - ${valoresTabuleiro[i+3]} - ${valoresTabuleiro[i+6]}`)
+            for (let id = i; id <= i+6; id+=3) {
+                btns[id].classList.add('marcado')
+            }
+            break
+        }
     }
 }
 
